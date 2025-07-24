499d5e52d668113ef8f597e8a871d7c982cc8b7a
wafaa <w.farag@speedi.sa>
2025-07-24 18:16:03 +0300

    Scrolling in product details view controller

--- StoreFront/Modules/ProductDetails/ProductDetailsViewController.swift
+++ StoreFront/Modules/ProductDetails/ProductDetailsViewController.swift
@@ -4,12 +4,16 @@
 //
 //  Created by wafaa farrag on 20/07/2025.
 //
+
 import UIKit
 import Kingfisher
 
-final class ProductDetailsViewController: BaseViewController {
+final class ProductDetailsViewController: BaseViewController, UIScrollViewDelegate {
     
+    @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var productImageView: UIImageView!
+    @IBOutlet private weak var imageHeightConstraint: NSLayoutConstraint!
+    
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var categoryLabel: UILabel!
     @IBOutlet private weak var priceLabel: UILabel!
@@ -18,17 +22,22 @@ final class ProductDetailsViewController: BaseViewController {
     
     var product: Product!
     
+    private var defaultImageHeight: CGFloat = 250 // initial height
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "productDetailsTitle".localized()
+        
+        scrollView.delegate = self
+        defaultImageHeight = imageHeightConstraint.constant
+        
         setupUI()
         configureProduct()
     }
     
     private func setupUI() {
-        productImageView.layer.cornerRadius = LayoutMetrics.cornerRadius
-        productImageView.layer.masksToBounds = true
         productImageView.contentMode = .scaleAspectFit
+        productImageView.clipsToBounds = true
         
         titleLabel.font = LayoutMetrics.Fonts.title
         categoryLabel.font = LayoutMetrics.Fonts.category
@@ -56,7 +65,7 @@ final class ProductDetailsViewController: BaseViewController {
         titleLabel.text = product.title
         categoryLabel.text = "categoryLabelPrefix".localized() + product.category.capitalized
         priceLabel.text = String(format: "priceFormat".localized(), product.price)
-  
+        
         let starsCount = Int(product.rating.rate.rounded())
         let stars = String(repeating: "⭐️", count: starsCount)
         ratingLabel.text = "\(stars) (\(product.rating.count))"
@@ -67,5 +76,15 @@ final class ProductDetailsViewController: BaseViewController {
             string: product.description,
             attributes: [.paragraphStyle: paragraph]
         )
+    }
+    
+    // MARK: - Stretchable Header Effect
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let yOffset = scrollView.contentOffset.y
+        if yOffset < 0 {
+            imageHeightConstraint.constant = defaultImageHeight - yOffset
+        } else {
+            imageHeightConstraint.constant = defaultImageHeight
+        }
     }
 }
